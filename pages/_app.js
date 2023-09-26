614277b7d92d53ebc5448394d623f29c173c97ce
kf2304072 <cometrue.6398@gmail.com>
2023-09-26 18:18:00 +0900

    公開準備

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '@/styles/globals.css'
+import "./mysite.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/about/index.js
@@ -0,0 +1,11 @@
+import Content from "../components/content";
+import Header from "../components/header";
+
+
+export default function About(){
+    return (
+        <Content>
+            <Header title="About" />
+        </Content>
+    )
+}
